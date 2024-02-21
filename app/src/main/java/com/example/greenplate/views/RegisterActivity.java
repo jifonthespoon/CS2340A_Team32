2df878539509134b21b707fdab3879836b521f56
jifonthespoon <austin.huguenard@gmail.com>
2024-02-20 21:33:37 -0500

    Add register activity

---
+++ app/src/main/java/com/example/greenplate/views/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.greenplate.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.greenplate.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
