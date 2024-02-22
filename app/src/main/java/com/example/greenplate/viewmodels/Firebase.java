87b37af2ddc5aec2f745076ad551df1c66091499
jifonthespoon <austin.huguenard@gmail.com>
2024-02-21 19:23:58 -0500

    Move from welcome screen to home or login

--- app/src/main/java/com/example/greenplate/viewmodels/Firebase.java
+++ app/src/main/java/com/example/greenplate/viewmodels/Firebase.java
@@ -1,4 +1,5 @@
 package com.example.greenplate.viewmodels;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -9,6 +10,9 @@ public class Firebase {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("https://greenplate-df836-default-rtdb.firebaseio.com/");
 
-    
+    static FirebaseAuth mAuth = FirebaseAuth.getInstance()
+    public static boolean isUserLoggedIn() {
+        return mAuth.getCurrentUser() != null;
+    }
 
 }
--- app/src/main/java/com/example/greenplate/views/MainActivity.java
+++ app/src/main/java/com/example/greenplate/views/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.greenplate.R;
+import com.example.greenplate.viewmodels.Firebase;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean userLoggedIn = Firebase.isUserLoggedIn();
+        if (userLoggedIn) {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
