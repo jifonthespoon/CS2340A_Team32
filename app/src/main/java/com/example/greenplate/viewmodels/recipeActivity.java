0fd81964339a48bc4ba5d7fd60eee9d192249c71
Sudhyakumar3 <sudhyakumar3@gatech.edu>
2024-02-21 22:02:02 -0500

    Add nav bar images, and recipe and shopping list header. Created recipe and shopping list pages.

---
+++ app/src/main/java/com/example/greenplate/viewmodels/recipeActivity.java
@@ -0,0 +1,21 @@
+package com.example.greenplate.viewmodels;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.greenplate.R;
+
+public class recipeActivity {
+
+    /**Button shoppingListNavButton = findViewById(R.id.shoppingListNavButton);
+        shoppingListNavButton.setOnClickListener(v -> {
+        Intent intent = new Intent(shoppingListNavButton.this, shopping_list.class);
+        startActivity(intent);
+    });
+     **/
+}
