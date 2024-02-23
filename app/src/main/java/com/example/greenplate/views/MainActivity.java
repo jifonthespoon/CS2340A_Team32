22269069a1fc9a142cbef709737a7f0094cd3c5a
kushal-dudipala <kushalreddyd@gmail.com>
2024-02-22 20:53:51 -0500

    add functionality to skeleton methods

--- app/src/main/java/com/example/greenplate/views/MainActivity.java
+++ app/src/main/java/com/example/greenplate/views/MainActivity.java
@@ -10,22 +10,6 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
-        boolean userLoggedIn = Firebase.isUserLoggedIn();
-        if (userLoggedIn) {
-            // Will be home activity when ready
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        }
-
-    }
-
 
 
 }
