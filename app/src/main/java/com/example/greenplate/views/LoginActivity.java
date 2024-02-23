d6e9df7facbccfe8122853f5cf21d511a9298799
kushal-dudipala <kushalreddyd@gmail.com>
2024-02-22 20:57:41 -0500

    changed method functionality

--- app/src/main/java/com/example/greenplate/views/LoginActivity.java
+++ app/src/main/java/com/example/greenplate/views/LoginActivity.java
@@ -21,5 +21,74 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView usernameTextView;
+    private TextView passwordTextView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen_activity);
+        usernameTextView = findViewById(R.id.usernameEditText);
+        passwordTextView = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+        Button signUpButton = findViewById(R.id.signUpButton);
+        Button exitButton = findViewById(R.id.exitButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String username = usernameTextView.getText().toString();
+                String password = passwordTextView.getText().toString();
+
+
+                if (checkInput(username) && checkInput(password)) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    FirebaseAuth mAuth = Firebase.getAuth();
+                    mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Home screen
+                                System.out.println("Success");
+                                Toast.makeText(LoginActivity.this, "Account found.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Account not found.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(LoginActivity.this, "Fill out all fields.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public static boolean checkInput(String text) {
+        if (text == null || text.trim().isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/greenplate/views/MainActivity.java
+++ app/src/main/java/com/example/greenplate/views/MainActivity.java
@@ -10,6 +10,20 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boolean userLoggedIn = Firebase.isUserLoggedIn();
+        if (userLoggedIn) {
+            // Will be home activity when ready
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
 
+    }
 
 }
