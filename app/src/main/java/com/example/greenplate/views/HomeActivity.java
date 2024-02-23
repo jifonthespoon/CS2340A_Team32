4444448a4b194bb3c271f5cefc60c0af5200d2d5
kushal-dudipala <kushalreddyd@gmail.com>
2024-02-23 00:33:12 -0500

    added functionality to skeleton methods

--- app/src/main/java/com/example/greenplate/views/HomeActivity.java
+++ app/src/main/java/com/example/greenplate/views/HomeActivity.java
@@ -1,19 +1,61 @@
 package com.example.greenplate.views;
 
+import com.example.greenplate.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-import com.example.greenplate.R;
-import com.example.greenplate.viewmodels.Firebase;
-import android.content.Intent;
-import android.util.Log;
+
 
 public class HomeActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final ImageButton toHome = findViewById(R.id.toHomePage);
+        final ImageButton toInput = findViewById(R.id.toInputPage);
+        final ImageButton toRecipe = findViewById(R.id.toRecpiePage);
+        final ImageButton toIngredients = findViewById(R.id.toIngredientsPage);
+        final ImageButton toShopping = findViewById(R.id.toShoppingPage);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        toHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        toInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+        toRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, RecipeActivity.class);
+                startActivity(intent);
+            }
+        });
+        toIngredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, IngredientsActivity.class);
+                startActivity(intent);
+            }
+        });
+        toShopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ShoppingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/greenplate/views/IngredientsActivity.java
+++ app/src/main/java/com/example/greenplate/views/IngredientsActivity.java
@@ -1,2 +1,60 @@
-package com.example.greenplate.views;public class IngredientsActivity {
+package com.example.greenplate.views;
+
+import com.example.greenplate.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class IngredientsActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+
+        final ImageButton toHome = findViewById(R.id.toHomePage);
+        final ImageButton toInput = findViewById(R.id.toInputPage);
+        final ImageButton toRecipe = findViewById(R.id.toRecpiePage);
+        final ImageButton toIngredients = findViewById(R.id.toIngredientsPage);
+        final ImageButton toShopping = findViewById(R.id.toShoppingPage);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ingredient_page);
+        toHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IngredientsActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        toInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IngredientsActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+        toRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IngredientsActivity.this, RecipeActivity.class);
+                startActivity(intent);
+            }
+        });
+        toIngredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IngredientsActivity.this, IngredientsActivity.class);
+                startActivity(intent);
+            }
+        });
+        toShopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IngredientsActivity.this, ShoppingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
