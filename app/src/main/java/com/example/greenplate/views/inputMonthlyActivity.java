86bd9127775dec53dec98628873d01e4416447bb
kushal-dudipala <kushalreddyd@gmail.com>
2024-03-10 17:14:57 -0400

    fix bugs and change button layout

--- app/src/main/java/com/example/greenplate/views/inputMonthlyActivity.java
+++ app/src/main/java/com/example/greenplate/views/inputMonthlyActivity.java
@@ -2,7 +2,9 @@ package com.example.greenplate.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
