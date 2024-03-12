637382c0eae699c2216784e0c8cfa8815b325350
nfritch <natefritchley@gmail.com>
2024-03-12 16:23:05 -0400

    Create data visualization framework from MPAndroidChart

--- app/src/main/java/com/example/greenplate/views/InputMonthlyActivity.java
+++ app/src/main/java/com/example/greenplate/views/InputMonthlyActivity.java
@@ -1,5 +1,9 @@
 package com.example.greenplate.views;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -10,14 +14,37 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.anychart.core.cartesian.series.Line;
 import com.example.greenplate.R;
+import com.example.greenplate.models.Firebase;
 import com.example.greenplate.models.Meal;
 import com.example.greenplate.viewmodels.FirebaseViewModel;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.Utils;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -125,6 +152,67 @@ public class InputMonthlyActivity extends AppCompatActivity {
             }
         });
 
+        LineChart mChart = findViewById(R.id.mpandroidchart1);
+        mChart.setTouchEnabled(true);
+        mChart.setPinchZoom(true);
+        mChart.getDescription().setText("");
+        ArrayList<Entry> values = new ArrayList<>();
+        values.add(new Entry(1, 2000));
+        values.add(new Entry(2, 1930));
+        values.add(new Entry(3, 1810));
+        values.add(new Entry(4, 2110));
+        values.add(new Entry(5, 2050));
+        values.add(new Entry(6, 1590));
+        values.add(new Entry(7, 1990));
+        values.add(new Entry(8, 2000));
+        values.add(new Entry(9, 1950));
+        values.add(new Entry(10, 1890));
+        values.add(new Entry(11, 2010));
+        values.add(new Entry(12, 2020));
+        values.add(new Entry(13, 1960));
+        values.add(new Entry(14, 1990));
+        LineDataSet set1;
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            set1.setDrawValues(false);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            set1 = new LineDataSet(values, "Calories per Day");
+            set1.setDrawIcons(false);
+            set1.setDrawValues(false);
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.DKGRAY);
+            set1.setCircleColor(Color.DKGRAY);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
 
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            LineData data = new LineData(dataSets);
+            mChart.setData(data);
+
+            YAxis leftAxis = mChart.getAxisLeft();
+            String caloriesGoalString = calorieGoal.getText().toString();
+            int recommendedDailyTotalCalories = Integer.parseInt(caloriesGoalString);
+
+
+            LimitLine ll = new LimitLine(recommendedDailyTotalCalories, "Recommended total calories");
+            ll.setLineColor(Color.RED);
+            ll.setLineWidth(2f);
+            ll.setTextColor(Color.BLACK);
+            ll.setTextSize(9f);
+
+            leftAxis.addLimitLine(ll);
+        }
     }
 }
