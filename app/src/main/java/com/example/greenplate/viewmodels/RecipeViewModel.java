361161d3c62cc5904c8d04269252a68519978330
Danielkim1211 <121706212+Danielkim1211@users.noreply.github.com>
2024-03-31 23:54:27 -0400

    Update RecipeViewModel.java

--- app/src/main/java/com/example/greenplate/viewmodels/RecipeViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/RecipeViewModel.java
@@ -60,53 +60,5 @@ public class RecipeViewModel implements RecipeNamesCallback {
     public static void removeRecipe(String recipeId) {
         firebase.getDatabase().getReference().child("cookbook").child(recipeId).removeValue();
     }
-    public static void fetchAllRecipesWithIndication(User user, ValueEventListener listener) {
-        DatabaseReference recipesRef = firebase.getDatabase().getReference().child("cookbook");
-        recipesRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                List<Recipe> recipesWithIndication = new ArrayList<>();
-                Map<String, Integer> userIngredientsQuantities = new HashMap<>();
-
-                // Convert the user's ingredients list to a map for easier comparison
-                for (Ingredient userIngredient : user.getIngredients()) {
-                    userIngredientsQuantities.put(userIngredient.getName().toLowerCase(),
-                            userIngredient.getQuantity());
-                }
-
-                for (DataSnapshot recipeSnapshot : snapshot.getChildren()) {
-                    Recipe recipe = recipeSnapshot.getValue(Recipe.class);
-                    boolean hasAllIngredients = true;
-
-                    for (Ingredient ingredient : recipe.getIngredients()) {
-                        String ingredientName = ingredient.getName().toLowerCase();
-                        int requiredQuantity = ingredient.getQuantity();
-
-                        if (!userIngredientsQuantities.containsKey(ingredientName)
-                                || userIngredientsQuantities.get(ingredientName)
-                                < requiredQuantity) {
-                            hasAllIngredients = false;
-                            break;
-                        }
-                    }
-
-                    // Optionally, use a custom object to hold the recipe and indication of
-                    // ingredient sufficiency
-                    if (hasAllIngredients) {
-                        recipesWithIndication.add(recipe);
-                    }
-                    // If you wish to show all recipes but mark those the user can cook, add all
-                    // to list but mark them differently
-                }
-
-                listener.onDataChange(snapshot); // Replace with passing recipesWithIndication to UI
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                listener.onCancelled(error);
-            }
-        });
-    }
 
 }
